fb79e07c2e1b74ac655de78d6f300413ff719beb
hihats <h_tsukamoto@innovation.co.jp>
2015-10-27 16:59:47 +0900

    Set builder on Base Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,11 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+  protected $client;
+  public function __construct()
+  {
+    $uinfo = array(getenv('SF_WSDL_FILE'),getenv('SF_API_USER'),getenv('SF_API_USER_PASSWD'),getenv('SF_API_TOKEN'));
+    $builder = new \Phpforce\SoapClient\ClientBuilder($uinfo[0],$uinfo[1],$uinfo[2],$uinfo[3]);
+    $this->client = $builder->build();
+  }
 }
